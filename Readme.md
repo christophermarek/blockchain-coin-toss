af43f0dc860676f9ca0827bd234ac14973fee658
christophermarek <christophermarek42@gmail.com>
2021-07-23 12:56:10 -0400

    barebones project

---
+++ Readme.md
@@ -0,0 +1,2 @@
+
+Secure coin toss game with react frontend and smart contract backend
---
+++ test/cointoss.js
@@ -0,0 +1,13 @@
+const cointoss = artifacts.require("cointoss");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("cointoss", function (/* accounts */) {
+  it("should assert true", async function () {
+    await cointoss.deployed();
+    return assert.isTrue(true);
+  });
+});
