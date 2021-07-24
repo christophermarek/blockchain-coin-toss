6a5ef5004d077b0d5d069f7d3f5323ef32f88830
christophermarek <christophermarek42@gmail.com>
2021-07-24 15:37:46 -0400

    before looping tests

--- test/cointest.js
+++ test/cointest.js
@@ -16,22 +16,41 @@ contract('cointoss', function (/* accounts */) {
     const player2address = '0x05Ea98B4e80277f43B8704fD93F96a4C8a9782f1';
     const player2wager = 1;
 
+    //PLAYER 1
     //test player one entered
-    it('player 1 entered', async function () {
+    it(`player 1 entered wagerIndex: ${wagerIndex}`, async function () {
         await instance.enterContest(wagerIndex, player1Wager, {
             from: player1address,
         })
-        let result = await instance.coinTossWagers(0);
+        let result = await instance.coinTossWagers(wagerIndex);
         //console.log(await instance.coinTossWagers(0));
-        console.log(Object.keys(result));
-        console.log(result.user1);
+        //console.log(Object.keys(result));
+        //console.log(result.user1);
         return assert.equal(player1address, result.user1);
-
         //console.log(await instance.coinTossWagers(0));
         //await cointoss.methods.coinTossWagers.call();
     })
 
-    //instance.enterContest(wagerIndex, player2Wager, { from: player2address });
+    it(`player 1 wager entered wagerIndex: ${wagerIndex}`, async function () {
+        let result = await instance.coinTossWagers(wagerIndex);
+        return assert.equal(player1Wager, result.user1wager);
+    })
+
+    //PLAYER 2
+    //test player two entered
+    it(`player 2 entered wagerIndex: ${wagerIndex}`, async function () {
+        await instance.enterContest(wagerIndex, player2wager, {
+            from: player2address,
+        })
+        let result = await instance.coinTossWagers(wagerIndex);
+        return assert.equal(player2address, result.user2);
+    })
+
+    it(`player 2 wager entered wagerIndex: ${wagerIndex}`, async function () {
+        let result = await instance.coinTossWagers(wagerIndex);
+        return assert.equal(player2wager, result.user2wager);
+    })
+
 
     //test entering contest if full.
 })
