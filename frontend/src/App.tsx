25fb814196a179e1dff6f5933972e205354a394b
christophermarek <christophermarek42@gmail.com>
2021-07-27 09:55:47 -0400

    usedapp setup kinda

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEthers, useEtherBalance } from '@usedapp/core';
+import { BigNumber } from 'ethers';
 
 function App() {
+  const STAKING_CONTRACT = '0x00000000219ab540356cBB839Cbe05303d7705Fa'
+
+  const { activateBrowserWallet, deactivate, account } = useEthers()
+  const userBalance = useEtherBalance(account)
+  const stakingBalance = useEtherBalance(STAKING_CONTRACT)
+
   return (
-    <div className="App">
-      <p>Hello</p>
+    <div>
+      {!account && <button onClick={() => activateBrowserWallet}> Connect </button>}
+      {account && <button onClick={deactivate}> Disconnect </button>}
+
+      {stakingBalance && <p>ETH2 staking balance: {formatEther(stakingBalance)} ETH </p>}
+      {account && <p>Account: {account}</p>}
+      {userBalance && <p>Ether balance: {formatEther(userBalance)} ETH </p>}
     </div>
-  );
+  )
 }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// index.tsx
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"
+// Import DAppProvider
+import { ChainId, DAppProvider } from '@usedapp/core'
+
+const config: any = {
+  readOnlyChainId: ChainId.Mainnet,
+  readOnlyUrls: {
+    [ChainId.Mainnet]: 'https://mainnet.infura.io/v3/62687d1a985d4508b2b7a24827551934',
+  },
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+)
